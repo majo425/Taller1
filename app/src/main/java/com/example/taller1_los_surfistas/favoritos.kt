ab2f01bea665d59cbb7c081cd59a8adfc2e40e4a
majo425 <mariacardenasm@javeriana.edu.co>
2024-08-29 18:43:15 -0500

    Ajuste

--- app/src/main/java/com/example/taller1_los_surfistas/favoritos.kt
+++ app/src/main/java/com/example/taller1_los_surfistas/favoritos.kt
@@ -22,18 +22,14 @@ class favoritos : AppCompatActivity() {
         // Obtener los destinos favoritos
         val favoritos = FavoriteDestinations.favoritesList
         val nombresFavoritos = favoritos.map { it.getString("nombre") }
-
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, nombresFavoritos)
         listViewFavoritos.adapter = adapter
-
         // Manejar clics en la lista de favoritos
         listViewFavoritos.setOnItemClickListener { parent, view, position, id ->
-            val destinoSeleccionado = favoritos[position] // Obtener el destino seleccionado
-            val intent = Intent(this, conFiltro::class.java) // Iniciar la actividad con la información del destino
+            val destinoSeleccionado = favoritos[position]
+            val intent = Intent(this, conFiltro::class.java)
             intent.putExtra("destinoNombre", destinoSeleccionado.getString("nombre"))
-            intent.putExtra("ocultarBotonFavoritos", true)  //Para ocultar el botón de "añadir a favoritos" que se encuentra en "conFiltro"
-            startActivity(intent)
+            intent.putExtra("ocultarBotonFavoritos", true)
         }
-
     }
 }
